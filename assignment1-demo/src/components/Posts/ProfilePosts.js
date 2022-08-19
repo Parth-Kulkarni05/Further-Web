a33ec91b1345274cae05615b7ad8b811c22c0b69
s3897250 <natnaelzg@gmail.com>
2022-08-19 10:44:05 +1000

    Added Posts section to repo

---
+++ assignment1-demo/src/components/Posts/ProfilePosts.js
@@ -0,0 +1,12 @@
+import { BrowserRouter, NavLink, Routes, Route, useNavigate, Link } from 'react-router-dom';
+import React, { useState, useEffect, Redirect, Navigate} from "react";
+
+
+function ProfilePosts() {
+
+
+
+}
+
+
+export default ProfilePosts
